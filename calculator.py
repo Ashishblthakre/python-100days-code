0f0e900109de9ec55d31f44f4811ab0a87ae0090
Ashishblthakre <ashishblthakre10@gmail.com>
2026-01-07 13:35:11 +0530

    Multiplication is added in the calculator

--- calculator.py
+++ calculator.py
@@ -3,3 +3,4 @@ b = 4
 
 print("The value of", a, "+", b, "is", a+b)
 print("The value of", a, "-",b, "is", a-b)
+print("The value of ", a, "*", b , "is", a*b)
---
+++ main.py
@@ -0,0 +1,9 @@
+# a = 12987
+# b = "Hare krishna"
+# print(a, b, sep = "\n")
+# a = input("Enter your name: ")
+# print("My name is ", type(a))
+
+x = input("Enter first number: ")
+y = input ("Enter second number:")
+print(x%y)
